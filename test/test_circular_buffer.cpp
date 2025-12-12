175f0496e75e18201cd10165f1055efd28dea437
John-ambrad <johnambrad001@gmail.com>
2025-12-12 16:03:46 -0500

    feature/write_flash006: basic init,read,write pass tests

--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -22,9 +22,57 @@ void test_circular_buffer_empty_after_init(){
 
 }
 
-void test_circular_buffer_add(){
+void test_circular_buffer_write(){
+
+    circular_buff_t buff;
+    circular_buff_init(&buff);
+
+
+
+    bma_accel_data_t mock_val = {1,2,3,1000};
+    circular_buff_write(mock_val,&buff);
+
+    TEST_ASSERT_EQUAL_INT(1,buff.head);
+    TEST_ASSERT_EQUAL_INT(1,buff.count);
+    TEST_ASSERT_EQUAL_INT(0,buff.tail);
+
 
 }
+
+
+void test_circular_buffer_read(){
+
+    circular_buff_t buff; 
+    bma_accel_data_t mock_val;
+
+
+    circular_buff_init(&buff);
+
+    // CREATE MOCK VALUES //
+    mock_val = {1,2,3,1000};
+
+    circular_buff_write(mock_val,&buff);
+    int count_exp = buff.count-1;
+    int tail_exp = buff.tail+1;
+
+
+
+
+    bma_accel_data_t read_val = circular_buff_read(&buff);
+
+
+    // check if tail is incrementing // 
+
+    TEST_ASSERT_EQUAL(1,read_val.x);
+    TEST_ASSERT_EQUAL(2,read_val.y);
+    TEST_ASSERT_EQUAL(3,read_val.z);
+    TEST_ASSERT_EQUAL(1000,read_val.timestamp_ms);
+    TEST_ASSERT_EQUAL(count_exp,buff.count);
+    TEST_ASSERT_EQUAL(tail_exp, buff.tail);
+
+
+}
+
 
 void test_circular_buffer_remove(){
 
@@ -50,18 +98,13 @@ void test_circular_buffer_clear(){
 
 }
 
-void test_circular_buffer_get(){
-
-
-
-
-}
-
 
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
 
     RUN_TEST(test_circular_buffer_empty_after_init);
+    RUN_TEST(test_circular_buffer_write);
+    RUN_TEST(test_circular_buffer_read);
     UNITY_END();
 }
