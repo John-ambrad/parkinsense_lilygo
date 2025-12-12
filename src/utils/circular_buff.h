37361fbbbedb0251c878aaacfc1a12bacc2b1c8e
John-ambrad <johnambrad001@gmail.com>
2025-12-12 13:42:03 -0500

    feature/write_flash002: created test stubs, writing basic circular buffer

---
+++ src/utils/circular_buff.h
@@ -0,0 +1,15 @@
+#ifndef CIRCULAR_BUFFER_H
+#define CIRCULAR_BUFFER_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+
+
+
+
+
+
+
+
+#endif //CIRCULAR_BUFFER_H
--- test/test_circular_buffer.cpp
+++ test/test_circular_buffer.cpp
@@ -1,4 +1,5 @@
 #include "unity.h"
+#include "../src/utils/circular_buff.h"
 
 void setUp(void){
 
@@ -7,6 +8,49 @@ void setUp(void){
 void tearDown(void){
 
 }
+
+//BASIC LOGIC AND FUNCTIONAL TESTING//
+void test_circular_buffer_empty_after_init(){
+
+
+}
+
+void test_circular_buffer_add(){
+
+}
+
+void test_circular_buffer_remove(){
+
+}
+
+void test_circular_buffer_full(){
+    //checks that the counter is implementing correctly 
+    //to test this have a fixed size circular buffer
+    //count == known fixed size then the counter is working properly
+    //also test to see that it will erase/overwrite old data?
+
+}
+
+void test_circular_buffer_overflow(){
+
+
+
+}
+
+void test_circular_buffer_clear(){
+
+
+
+}
+
+void test_circular_buffer_get(){
+
+
+
+
+}
+
+
 
 int main(int argc, char **argv) {
     UNITY_BEGIN();
